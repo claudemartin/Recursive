20d85c46472d935de75c4eb712379432a7f97970
claudemartin <claude.martin@students.fhnw.ch>
2015-05-12 15:36:31 +0200

    simpler Demo.

--- Recursive/src/ch/claude_martin/recursive/Demo.java
+++ Recursive/src/ch/claude_martin/recursive/Demo.java
@@ -4,22 +4,12 @@ import java.util.function.IntToLongFunction;
 import java.util.stream.IntStream;
 
 public class Demo {
-  /** Since Java 8 still doesn't have predefined tuples... */
-  static class FibResult {
-    long n, Fn;
-
-    public FibResult(long n, long fn) {
-      this.n = n;
-      this.Fn = fn;
-    }
-  }
-
   public static void main(String[] args) throws Throwable {
     // "fib" is a recursive function.
     final IntToLongFunction fib = Recursive.cachedIntToLongFunction(//
         (n, self) -> n <= 1 ? n : self.applyAsLong(n - 1) + self.applyAsLong(n - 2), 0, 60);
 
-    IntStream.rangeClosed(0, 60).mapToObj(n -> new FibResult(n, fib.applyAsLong(n)))//
-        .forEach(r -> System.out.format("F%d = %d%n", r.n, r.Fn));
+    IntStream.rangeClosed(0, 60).mapToObj(n -> String.format("F%d = %d", n, fib.applyAsLong(n)))
+        .forEach(System.out::println);
   }
 }
