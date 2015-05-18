69e8fa34f9e35f3fd83196d8650eca8bfcb28196
claudemartin <claude.martin@students.fhnw.ch>
2015-05-18 12:51:57 +0200

    weak cache now uses weak references for keys too.

--- Recursive/src/ch/claude_martin/recursive/cache/FunctionCache.java
+++ Recursive/src/ch/claude_martin/recursive/cache/FunctionCache.java
@@ -3,6 +3,7 @@ package ch.claude_martin.recursive.cache;
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.WeakHashMap;
 import java.util.function.Supplier;
 
 @FunctionalInterface
@@ -17,8 +18,9 @@ public interface FunctionCache<T, R> {
     return create(HashMap::new);
   }
 
+  /** Cache which only holds weak references (for both input and output). */
   public static <T, R> FunctionCache<T, R> createWeakCache() {
-    final HashMap<T, WeakReference<R>> map = new HashMap<>();
+    final WeakHashMap<T, WeakReference<R>> map = new WeakHashMap<>();
     return (t, s) -> {
       R result = null;
       final WeakReference<R> ref = map.computeIfAbsent(t, key -> new WeakReference<>(s.get()));
